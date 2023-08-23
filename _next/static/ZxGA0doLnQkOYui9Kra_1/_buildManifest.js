9f31dfe9a85cf5b6829fbac4940b5ccbff2e4218
Gabriel Lins <gabrielinsmelo@gmail.com>
2023-08-22 23:11:03 -0300

    Updates

--- _next/static/ZxGA0doLnQkOYui9Kra_1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e8134bb747c27d3f.css","static/chunks/pages/index-ac44a06c9e2b92f8.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
