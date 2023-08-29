b833281a5b8ad4f44fcb0cdf4c075b8ded1b0819
Gabriel Lins <gabrielinsmelo@gmail.com>
2023-08-29 14:08:39 -0300

    feat: :necktie: update navbar links

--- src/components/LamacNavbar/LamacNavbar.tsx
+++ src/components/LamacNavbar/LamacNavbar.tsx
@@ -15,28 +15,50 @@ function LamacNavbar({ brandDelayed = false }) {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
+            {/* Home */}
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">Sobre</Nav.Link>
+
+            {/* About */}
+            <Nav.Link href="/about" className='d-lg-none'>Sobre o LaMaC</Nav.Link>
             <NavDropdown
-              title="Linhas de Pesquisa"
+              title="Informações"
               id="research-nav-dropdown"
               renderMenuOnMount={true}
+              onToggle={() => window.location.href='/about'}
+              className='d-none d-lg-block'
             >
-              <NavDropdown.Item href="/research/computer-graphics">Computação Gráfica</NavDropdown.Item>
-              <NavDropdown.Item href="/research/image-processing">Processamento de Imagens</NavDropdown.Item>
-              <NavDropdown.Item href="/research/machine-learning">Aprendizado de Máquina</NavDropdown.Item>
+              <NavDropdown.Item href="/about#lamac">Sobre o LaMaC</NavDropdown.Item>
+              <NavDropdown.Item href="/about#computational-math">O que é Matemática Computacional</NavDropdown.Item>
+              <NavDropdown.Item href="/about#infrastructure">Infraestrutura do laboratório</NavDropdown.Item>
+              <NavDropdown.Item href="/about#research-areas">Linhas de pesquisa</NavDropdown.Item>
+              <NavDropdown.Item href="/about#team">Nossa equipe</NavDropdown.Item>
             </NavDropdown>
 
+            {/* Tutorials */}
+            <Nav.Link href="/about" className='d-lg-none'>Tutoriais</Nav.Link>
             <NavDropdown
-              title="Tutoriais"
               id="tutorials-nav-dropdown"
+              title="Tutoriais"
               renderMenuOnMount={true}
+              onToggle={() => window.location.href='/tutorials'}
+              className='d-none d-lg-block'
             >
-              <NavDropdown.Item href="/tutorials/math">Matemática</NavDropdown.Item>
-              <NavDropdown.Item href="/tutorials/">Programação</NavDropdown.Item>
+              <NavDropdown.Header>Matemática</NavDropdown.Header>
+              <NavDropdown.Item href="/tutorials/linear-algebra">Álgebra Linear</NavDropdown.Item>
+              <NavDropdown.Item href="/tutorials/numerical-calculus">Cálculo Numérico</NavDropdown.Item>
+              <NavDropdown.Divider></NavDropdown.Divider>
+              <NavDropdown.Header>Programação</NavDropdown.Header>
+              <NavDropdown.Item href="/tutorials/data-visualization">Visualização de Dados (Matplotlib)</NavDropdown.Item>
+              <NavDropdown.Item href="/tutorials/image-processing">Processamento de Imagens (OpenCV)</NavDropdown.Item>
+              <NavDropdown.Item href="/tutorials/computer-graphics">Computação Gráfica (WebGL)</NavDropdown.Item>
+              <NavDropdown.Item href="/tutorials/machine-learning">Machine Learning (PyTorch)</NavDropdown.Item>
             </NavDropdown>
 
-            <Nav.Link href="/contact-us">Contato</Nav.Link>
+            {/* Productions */}
+            <Nav.Link href="/productions">Produções</Nav.Link>
+
+            {/* Contact */}
+            <Nav.Link href="#contact-us">Contato</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
