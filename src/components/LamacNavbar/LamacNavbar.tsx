8f407c732b58623252a08336ca254f5c281f0374
Gabriel Lins <gabrielinsmelo@gmail.com>
2023-08-29 09:32:27 -0300

    feat: :lipstick: fix navbar height

--- src/components/LamacNavbar/LamacNavbar.tsx
+++ src/components/LamacNavbar/LamacNavbar.tsx
@@ -6,43 +6,41 @@ import styles from './LamacNavbar.module.css';
 
 function LamacNavbar({ brandDelayed = false }) {
   return (
-    <div>
-      <Navbar className={styles.Navbar} expand="lg">
-        <Container>
-          <Navbar.Brand href='/' className={brandDelayed ? styles.DelayedNavBrand : ''}>
-            <Image src={brand} width={250} alt="LaMaC Logo White" priority />
-          </Navbar.Brand>
+    <Navbar className={styles.Navbar} expand="lg">
+      <Container>
+        <Navbar.Brand href='/' className={brandDelayed ? styles.DelayedNavBrand : ''}>
+          <Image src={brand} width={250} alt="LaMaC Logo White" priority />
+        </Navbar.Brand>
 
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/about">Sobre</Nav.Link>
-              <NavDropdown
-                title="Linhas de Pesquisa"
-                id="research-nav-dropdown"
-                renderMenuOnMount={true}
-              >
-                <NavDropdown.Item href="/research/computer-graphics">Computação Gráfica</NavDropdown.Item>
-                <NavDropdown.Item href="/research/image-processing">Processamento de Imagens</NavDropdown.Item>
-                <NavDropdown.Item href="/research/machine-learning">Aprendizado de Máquina</NavDropdown.Item>
-              </NavDropdown>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">Sobre</Nav.Link>
+            <NavDropdown
+              title="Linhas de Pesquisa"
+              id="research-nav-dropdown"
+              renderMenuOnMount={true}
+            >
+              <NavDropdown.Item href="/research/computer-graphics">Computação Gráfica</NavDropdown.Item>
+              <NavDropdown.Item href="/research/image-processing">Processamento de Imagens</NavDropdown.Item>
+              <NavDropdown.Item href="/research/machine-learning">Aprendizado de Máquina</NavDropdown.Item>
+            </NavDropdown>
 
-              <NavDropdown
-                title="Tutoriais"
-                id="tutorials-nav-dropdown"
-                renderMenuOnMount={true}
-              >
-                <NavDropdown.Item href="/tutorials/math">Matemática</NavDropdown.Item>
-                <NavDropdown.Item href="/tutorials/">Programação</NavDropdown.Item>
-              </NavDropdown>
+            <NavDropdown
+              title="Tutoriais"
+              id="tutorials-nav-dropdown"
+              renderMenuOnMount={true}
+            >
+              <NavDropdown.Item href="/tutorials/math">Matemática</NavDropdown.Item>
+              <NavDropdown.Item href="/tutorials/">Programação</NavDropdown.Item>
+            </NavDropdown>
 
-              <Nav.Link href="/contact-us">Contato</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+            <Nav.Link href="/contact-us">Contato</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
