308d9467752373a9d563f0fa84c7c53436e9ed82
Gabriel Lins <gabrielinsmelo@gmail.com>
2023-08-30 14:53:27 -0300

    refactor: :recycle: update global styling priority order

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
-import '../static/styles/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../static/styles/styles.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
