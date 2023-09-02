dab674009b126e9aa6632bb00428729d8d9204ca
Gabriel Lins <gabrielinsmelo@gmail.com>
2023-09-02 11:20:29 -0300

    refactor: :recycle: add webgl code completion and jsdocs to webgl API

--- src/static/ts/cg/webglAPI.js
+++ src/static/ts/cg/webglAPI.js
@@ -1,4 +1,12 @@
+/** @type {WebGLRenderingContext} */
 
+/**
+ * Function to create a shader
+ * @param {WebGLRenderingContext} gl - WebGL rendering context
+ * @param {number} type - The type of shader (gl.VERTEX_SHADER or gl.FRAGMENT_SHADER)
+ * @param {string} source - The GLSL source code.
+ * @returns {WebGLShader}
+ */
 export function createShader(gl, type, source) {
   var shader = gl.createShader(type);
   gl.shaderSource(shader, source);
@@ -8,10 +16,18 @@ export function createShader(gl, type, source) {
     return shader;
   }
 
-  console.log(gl.getShaderInfoLog(shader));
+  let err = gl.getShaderInfoLog(shader);
   gl.deleteShader(shader);
+  throw new Error(`Failed to create shader! Error: "${err}"`);
 }
 
+/**
+ * Function to create a program
+ * @param {WebGLRenderingContext} gl - WebGL rendering context
+ * @param {WebGLShader} vertexShader - WebGL vertex Shader
+ * @param {WebGLShader} fragmentShader - WebGL fragment Shader
+ * @returns {WebGLProgram}
+ */
 export function createProgram(gl, vertexShader, fragmentShader) {
   var program = gl.createProgram();
   gl.attachShader(program, vertexShader);
@@ -22,6 +38,7 @@ export function createProgram(gl, vertexShader, fragmentShader) {
     return program;
   }
 
-  console.log(gl.getProgramInfoLog(program));
+  let err = gl.getProgramInfoLog(program);
   gl.deleteProgram(program);
+  throw new Error(`Failed to create program! Error: "${err}"`);
 }
--- src/static/ts/cg/webglHw.js
+++ src/static/ts/cg/webglHw.js
@@ -18,23 +18,29 @@ const fragmentShaderSource = `
 `
 
 export function helloWorldGL(canvas) {
+  /** @type {WebGLRenderingContext} */
   var gl = canvas.getContext("webgl");
   if (!gl) {
     return;
   }
 
-  // Get the strings for our GLSL shaders and
-  // create GLSL shaders, upload the GLSL source, compile the shaders
-  var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-  var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-  
-  // Link the two shaders into a program
-  var program = createProgram(gl, vertexShader, fragmentShader);
+  try {
+    // Get the strings for our GLSL shaders and create GLSL shaders,
+    // upload the GLSL source, compile the shaders
+    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+    // Link the two shaders into a program
+    var program = createProgram(gl, vertexShader, fragmentShader);
+  } catch (error) {
+    console.error(error);
+    return;
+  }
 
   // look up where the vertex data needs to go.
   var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 
-  // Create a buffer and bind it to ARRAY_BUFFER 
+  // Create a buffer and bind it to ARRAY_BUFFER
   var positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer); // think of it as ARRAY_BUFFER = positionBuffer
 
@@ -47,7 +53,7 @@ export function helloWorldGL(canvas) {
 
   // put three 2d clip space points in the positionBuffer
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-  
+
   // code above this line is initialization code.
   // code below this line is rendering code.
 
@@ -59,16 +65,16 @@ export function helloWorldGL(canvas) {
   // Clear the canvas
   gl.clearColor(0, 0, 0, 0);
   gl.clear(gl.COLOR_BUFFER_BIT);
-  
+
   // Tell it to use our program (pair of shaders)
   gl.useProgram(program);
 
   // Turn on the attribute
   gl.enableVertexAttribArray(positionAttributeLocation);
-  
+
   // Bind the position buffer.
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-   
+
   // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
   var size = 2;          // 2 components per iteration
   var type = gl.FLOAT;   // the data is 32bit floats
