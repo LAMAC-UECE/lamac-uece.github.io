3c65b51d99aacee364baf71c7851d791afe8c5fb
Gabriel Lins <gabrielinsmelo@gmail.com>
2023-09-03 11:44:40 -0300

    feat: :sparkles: using pixels instead clip space (change vertex shader processing to convert to clip space)

--- src/static/ts/cg/webglHw.js
+++ src/static/ts/cg/webglHw.js
@@ -2,12 +2,22 @@ import { createProgram, createShader } from "./webglAPI";
 import webglUtils from "./webgl-utils";
 
 const vertexShaderSource = `
-  attribute vec4 a_position;
+  attribute vec2 a_position;
+  uniform vec2 u_resolution;
 
   void main() {
-    gl_Position = a_position;
+    // convert the position from pixels to 0.0 to 1.0
+    vec2 zeroToOne = a_position / u_resolution;
+
+    // convert from 0->1 to 0->2
+    vec2 zeroToTwo = zeroToOne * 2.0;
+
+    // convert from 0->2 to -1->+1 (clip space)
+    vec2 clipSpace = zeroToTwo - 1.0;
+
+    gl_Position = vec4(clipSpace, 0, 1);
   }
-`
+`;
 
 const fragmentShaderSource = `
   precision mediump float;
@@ -15,7 +25,7 @@ const fragmentShaderSource = `
   void main() {
     gl_FragColor = vec4(1, 0, 0.5, 1);
   }
-`
+`;
 
 export function helloWorldGL(canvas) {
   /** @type {WebGLRenderingContext} */
@@ -39,6 +49,7 @@ export function helloWorldGL(canvas) {
 
   // look up where the vertex data needs to go.
   var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
 
   // Create a buffer and bind it to ARRAY_BUFFER
   var positionBuffer = gl.createBuffer();
@@ -46,9 +57,9 @@ export function helloWorldGL(canvas) {
 
   // three 2d points
   var positions = [
-    0, 0,
-    0, 0.5,
-    0.7, 0,
+    250, 200,
+    400, 250,
+    250, 300,
   ];
 
   // put three 2d clip space points in the positionBuffer
@@ -68,6 +79,9 @@ export function helloWorldGL(canvas) {
 
   // Tell it to use our program (pair of shaders)
   gl.useProgram(program);
+
+  // set the resolution
+  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
   // Turn on the attribute
   gl.enableVertexAttribArray(positionAttributeLocation);
