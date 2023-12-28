69153b259404911ac453416620f41797b66fdc51
Gabriel Lins <gabrielinsmelo@gmail.com>
2023-12-28 14:33:18 -0300

    feat: :sparkles: add configs gear in all pages

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,20 +1,40 @@
-import React from 'react'
-import styles from './contact.module.css'
+"use client";
+
+import React, { useState } from "react";
+import styles from "./contact.module.css";
+import ConfigsGear from "../../components/ConfigsGear/ConfigsGear";
+import ConfigsModal from "../../components/ConfigsModal/ConfigsModal";
 
 function Contact() {
+  const [isConfigsModalVisible, setConfigsModalVisible] = useState(false);
+
   return (
     <div className={styles.ContactPageContainer}>
       <div>
-      <h2>Fale conosco</h2>
-      <div>
-        <p>Para entrar em contato envie um email para:</p>
-        <p><a href="mailto:thelmo.araujo@uece.br">thelmo.araujo@uece.br</a></p>
-        <p>ou</p>
-        <p><a href="mailto:gabriel.lins@aluno.uece.br">gabriel.lins@aluno.uece.br</a></p>
+        <h2>Fale conosco</h2>
+        <div>
+          <p>Para entrar em contato envie um email para:</p>
+          <p>
+            <a href="mailto:thelmo.araujo@uece.br">thelmo.araujo@uece.br</a>
+          </p>
+          <p>ou</p>
+          <p>
+            <a href="mailto:gabriel.lins@aluno.uece.br">
+              gabriel.lins@aluno.uece.br
+            </a>
+          </p>
+        </div>
       </div>
-      </div>
+      <ConfigsGear
+        delayed={false}
+        toggleConfigsModalVisibility={setConfigsModalVisible}
+      />
+      <ConfigsModal
+        show={isConfigsModalVisible}
+        setShow={setConfigsModalVisible}
+      />
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,31 @@
-"use client"
+"use client";
 
-import { Container } from 'react-bootstrap';
-import styles from './not-found.module.css';
+import { Container } from "react-bootstrap";
+import styles from "./not-found.module.css";
+import { useState } from "react";
+import ConfigsGear from "../components/ConfigsGear/ConfigsGear";
+import ConfigsModal from "../components/ConfigsModal/ConfigsModal";
 
 export default function Custom404() {
+  const [isConfigsModalVisible, setConfigsModalVisible] = useState(false);
+
   return (
-    <>
+    <div className={styles.NotFoundPageContainer}>
       <Container className={styles.NotFoundContainer}>
-        <div className='text-center'>
+        <div className="text-center">
           <h1>Ops...</h1>
           <h3>Esta página não existe.</h3>
-          <a href="/">
-            Voltar à página inicial
-          </a>
+          <a href="/">Voltar à página inicial</a>
         </div>
       </Container>
-    </>
+      <ConfigsGear
+        delayed={false}
+        toggleConfigsModalVisibility={setConfigsModalVisible}
+      />
+      <ConfigsModal
+        show={isConfigsModalVisible}
+        setShow={setConfigsModalVisible}
+      />
+    </div>
   );
 }
